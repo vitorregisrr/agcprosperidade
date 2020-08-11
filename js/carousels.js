2cbe319c5dc7e286edf66ec9d514f5c5d5cdddb2
vitorregisr <vitorregisrr@gmail.com>
2020-08-10 21:58:09 -0300

    Misc improves

--- js/carousels.js
+++ js/carousels.js
@@ -1,5 +1,26 @@
 (function () {
     'use strict';
     
+    // Home Carousel //
+    const loginCarousel = $("#login-carousel");
+    loginCarousel.owlCarousel({
+        loop: true,
+        autoplay: true,
+        margin: 0,
+        items: 1,
+        autoHeight: true,
+        autoHeightClass: 'owl-height',
+        center: true,
+        nav: false,
+        dots: true,
+        startPosition: 0,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        dotsContainer: '#login-carousel-dots',
+        onChange: function(e){
+
+        },
+    });
+    
     window.dispatchEvent(new Event('resize'));
 })();

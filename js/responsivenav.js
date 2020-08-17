cafec0f50129dceefabe4efe449e4efb1a031be3
vitorregisr <vitorregisrr@gmail.com>
2020-08-17 02:28:11 -0300

    Misc improves

--- js/responsivenav.js
+++ js/responsivenav.js
@@ -2,13 +2,12 @@
     'use strict';
 
     $('[data-toggle="navbar"]').click(function () {
+        $('.home-tree__cookies').addClass('hide');
         const isExpanded = $(this).attr('aria-expanded') === 'false' ? false : true;
 
         $('#mobile-nav-content').toggleClass('active');
-
         $(this).attr('aria-expanded', !isExpanded);
         $('.home-tree').removeClass('has-cookies');
-        $('.home-tree__cookies').addClass('hide');
     });
     
     window.dispatchEvent(new Event('resize'));

88f6fa8f1f8a2a02a370c5b164ae59d6dfbe72a6
vitorregisr <vitorregisrr@gmail.com>
2020-08-19 21:28:04 -0300

    Misc improves

--- js/misc.js
+++ js/misc.js
@@ -93,5 +93,37 @@
         }
     });
 
+     //Count up script
+     function checkVisible(elm) {
+        var rect = elm.getBoundingClientRect();
+        var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+        return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
+      }
+     $(window).scroll(function () {
+        $('.count').each(function () {
+            if (checkVisible(this)) {
+                
+                const $this = $(this);
+                if ($this.attr('data-counted') == 'false') {
+                    $this.attr('data-counted', true);
+        
+                    setTimeout(function () {
+                        jQuery({
+                            Counter: 0
+                        }).animate({
+                            Counter: $this.text()
+                        }, {
+                            duration: parseInt($this.attr('data-duration')) || 2000,
+                            easing: 'swing',
+                            step: function () {
+                                $this.text(Math.ceil(this.Counter));
+                            }
+                        });
+                    }, $this.attr('data-delay') || 200);
+                }
+            }
+        })
+    })
+
     window.addEventListener("resize", replaceStyles);
 })();
